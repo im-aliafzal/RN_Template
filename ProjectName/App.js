1d380bea2a5493e84a5e5830753da31773df7773
Ali Hashmi <im.alihashmi14@outlook.com>
2021-10-27 16:11:09 +0500

    Settings some components

--- ProjectName/App.js
+++ ProjectName/App.js
@@ -6,22 +6,22 @@
  * @flow strict-local
  */
 
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {LogBox, StyleSheet, Text, useColorScheme, View} from 'react-native';
+import {LogBox} from 'react-native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
-import globalStyles from 'src/config/globalStyles';
+import HomeStack from 'src/Navigations/HomeStack';
 import {persistor, store} from 'src/Redux/ConfigureStore';
-
 LogBox.ignoreAllLogs();
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <View style={{flex: 1, width: '100%', ...globalStyles.center}}>
-          <Text>HELLO</Text>
-        </View>
+        <NavigationContainer>
+          <HomeStack />
+        </NavigationContainer>
       </PersistGate>
     </Provider>
   );
--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 #!/usr/bin/env node
 
 console.log("This is post init script");
+var inquirer = require("inquirer");
+const output = [];
+
+const questions = [
+  {
+    type: "input",
+    name: "tvShow",
+    message: "What's your favorite TV show?",
+  },
+  {
+    type: "confirm",
+    name: "askAgain",
+    message: "Want to enter another TV show favorite (just hit enter for YES)?",
+    default: true,
+  },
+];
+
+function ask() {
+  inquirer.prompt(questions).then((answers) => {
+    output.push(answers.tvShow);
+    if (answers.askAgain) {
+      ask();
+    } else {
+      console.log("Your favorite TV Shows:", output.join(", "));
+    }
+  });
+}
+
+ask();
